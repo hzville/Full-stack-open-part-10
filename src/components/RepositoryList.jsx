94f66b43914fe0991c28e6a734397c0635f9ab46
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-11-03 01:02:11 +0200

    10.3

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,23 @@
+import { FlatList, View, StyleSheet } from 'react-native';
+import RepositoryItem from './RepositoryItem';
+import { repositories } from '../data/repositories';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
+
+const RepositoryList = () => {
+  return (
+    <FlatList
+      data={repositories}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({item}) => <RepositoryItem item={item}/>}
+    />
+  );
+};
+
+export default RepositoryList;
