ad6c2478c065b7aa7a2a5c40b9d17ccc4f29a976
Ville Halonen <71012693+hzville@users.noreply.github.com>
2024-11-06 20:51:57 +0200

    redo folder structure

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, ScrollView, Text } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../themes/theme';
 import AppBarTab from './AppBarTab';
